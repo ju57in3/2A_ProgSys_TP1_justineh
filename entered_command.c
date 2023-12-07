84666e1eed0b473ebe5300f57afcc74bafb781f4
ju57in3 <152391298+ju57in3@users.noreply.github.com>
2023-12-07 09:40:10 +0100

    Update entered_command.c

--- entered_command.c
+++ entered_command.c
@@ -6,19 +6,21 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <sys/types.h>
+//#include <sys/types.h>
+
+const void *bye = "Goodbye!\n";
 
 void Command(char *command){
-    pid_t pid = fork();
+    pid_t pid = fork();                                  //fork a new process
     if(pid == -1){
         perror("fork");
         exit(EXIT_FAILURE);
-    } else if(pid == 0){
+    } else if(pid == 0){                                 //child process
         execlp(command, command, (char *)NULL);
-        perror("execlp");
+        perror("execlp");                             //if execlp fails
         exit(EXIT_FAILURE);
-    } else {
-        waitpid(pid, NULL, 0);
+    } else {                                             //parent process
+        waitpid(pid, NULL, 0);            //wait for the child to finish
     }
 }
 
@@ -26,21 +28,32 @@ void WelcomeMessage(){
     const void *message= "Welcome in ENSEA tiny Shell.\nType 'exit' to quit.\n";
 
     //Display of a welcome message
-    ssize_t bytesWrite = write(STDOUT_FILENO, message, strlen(message));
+    /*ssize_t bytesWrite = */write(STDOUT_FILENO, message, strlen(message));
 }
 
 void Prompt(){
-    const void *prompt="enseash %";
+    const void *prompt="enseash % ";
 
     //Display of a simple prompt
-    ssize_t bytesWrite = write(STDOUT_FILENO, prompt, strlen(prompt));
+    /*ssize_t bytesWrite = */write(STDOUT_FILENO, prompt, strlen(prompt));
 }
 
-int main2(){
+int main(){
     WelcomeMessage();
     while(1){
         Prompt();
-        ssize_t bytesRead = read(STDIN_FILENO,);
-        Command();
+        char userInput[1024];
+        fgets(userInput,1024,stdin);
+
+        //remove new line character from user input
+        userInput[strcspn(userInput,"\n")]='\0';
+
+        //check if the user wants to exit
+        if(strcmp(userInput,"exit")==0){
+            write(STDOUT_FILENO,bye,strlen(bye));
+        }
+
+        //execute the entered command
+        Command(userInput);
     }
 }
